8ee674110dd0a82d8393c0398ebb4fd472c1c960
Isabel Ketner <isabelketner@gmail.com>
2020-11-20 15:25:33 -0800

    clean up crew

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# Wellness and Productivity (WAP)
+# wellness and productivity (WAP)
 Michelle Lam, Michelle Tan, Jackie Lam, & Isabel Ketner
 
 ## overview
 WAP is a device that aims to improve one's physical and mental health via personalized wellness exercises the user can be reminded to complete throughout the day.
 
-## setup
+## structure
+main.py  //runs application on laptop \
+helper.py //helper functions + constants for main.py \
+rpi-main.py //driving function on raspi \
+imu //rpi \
++-- python-BerryIMU-gryo-accel-compass-filters \
++-- +-- berryIMU_classifier.py \
+mqtt //rpi + laptop \
++-- pub.py \
++--sub.py \
+matrix //rpi \
++--matrix_functions.py \
+speech //laptop \
++-- audio_msg.py \
+ui //laptop \
++-- screen.kv
 
-### ui
+## setup
 within your conda environment, run \
 `conda install -c conda-forge wheel` \
 `conda install -c anaconda pip` \
 `python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew` \
 `python -m pip install kivy.deps.gstreamer` \
-`python –m pip install kivy`
+`python –m pip install kivy`\
+`conda install -c conda-forge paho-mqtt`
--- helper.py
+++ helper.py
@@ -1,6 +1,40 @@
+from MQTT.sub import client_mqtt
+
+### CONSTANTS ###
+
 #minutes
-stretch_reminder = .1
+stretch_reminder = .5
 breath_reminder = .5
 talk_reminder = 1
 
 congrats_message ="Congrats on completing a task!\nYou will be reminded to complete more tasks throughout the day."
+
+### HELPER FUNCTIONS ###
+
+def activate(activity):
+    #TODO: use MQTT to let RPi know to light em tf up
+    print("send message to LED Matrix")
+
+    #listen on MQTT broker to see if any gestures are being detected at the current moment
+    print("waiting for IMU activation for " + activity)
+    client_instance = client_mqtt('/team2/imu')
+    caliente = client_instance.connect_mqtt()
+    client_instance.subscribe_msg(caliente)
+    caliente.loop_start()
+    while(client_instance.message == ''): #while(client_instance.message != 'activation msg') ??
+        pass
+    caliente.disconnect()
+
+    if activity == 'strech':
+        #TODO: call stretching function
+        print("calling " + activity + " exercise")
+    if activity == 'breath':
+        #TODO: call breathing function
+        print("calling " + activity + " exercise")
+    if activity == 'talk':
+        #TODO: call talking to friends function
+        print("calling " + activity + " exercise")
+
+def congrats (activity):
+    #TODO: let users in network know you finished activity
+    print("letting friends know you finished an activity")
