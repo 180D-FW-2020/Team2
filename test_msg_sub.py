6146f4ce21b93cd9aca172c51c90664135761e4f
michellelaam <>
2020-12-03 17:00:56 -0800

    Testing 'talking to ffriends' subscriber

--- test_msg_sub.py
+++ test_msg_sub.py
@@ -2,15 +2,14 @@ from MQTT.sub import client_mqtt
 
 def listen():
     print("listening on topic for reminders!")
-    client_instance = client_mqtt('/team2/reminders')
+    client_instance = client_mqtt('/team2/messages')
     caliente = client_instance.connect_mqtt()
     client_instance.subscribe_wav(caliente)
+    client_instance.subscribe_txt(caliente)
     caliente.loop_start()
     while True:
         if(client_instance.message == 'talk'):
-            print('received audio msg')
-            run_reminder()
-        client_instance.set_message('')
+            print('receiving message + transcript')
 
 if __name__ == "__main__":
     listen()
