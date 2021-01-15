696db5b04d301512388aec5446b0e2dd723d15b3
Michelle Tan <michellektan37@gmail.com>
2021-01-14 20:39:58 -0800

    audio and txt created

--- test_msg_sub.py
+++ test_msg_sub.py
@@ -1,5 +1,6 @@
 from MQTT.sub import client_mqtt
 from os import path
+import time
 
 f = open('ID.txt', 'r')
 user_id = f.readline().replace('\n', '')
@@ -12,15 +13,17 @@ txt_suffix = "txt"
 audio_suffix = "wav"
 
 def listen():
-    client_instance = client_mqtt(audio_topic)
+    client_instance = client_mqtt(txt_topic, audio_topic)
     client_instance.get_topics()
     caliente = client_instance.connect_mqtt()
-    client_instance.subscribe_file(caliente, "testing.wav")
+    client_instance.subscribe_file(caliente, "test.wav")
     caliente.loop_start()
-    for i in range(0,10):
-        while(client_instance.message == ''):
-            pass
-        print("client msg:" , client_instance.message)
+    while(client_instance.message == ''):
+        if path.exists("test.wav"):
+            time.sleep(10)
+            print("found .wav - save in .txt")
+            if not path.exists("test.txt"):
+                client_instance.subscribe_file(caliente, "test.txt")
 
     caliente.disconnect()
 
