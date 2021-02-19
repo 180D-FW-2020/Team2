de726b9fb32e4baa41991e9c9cb94897f5a8d238
michellektan <michellektan37@gmail.com>
2021-02-19 00:48:43 +0000

    install rpi script

--- install_rpi.sh
+++ install_rpi.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-conda create --name W_RPI --force -y
+echo "Activating conda environment"
+conda create --name WAP --force -y
+conda config --set report_errors false
 eval "$(conda shell.bash hook)"
-conda activate W_RPI
+conda activate WAP
 
-echo "Installing python==3.7.6"
-conda install -y python==3.6.1
-
-echo "Installing git"
-conda install -y -c anaconda git
-
-git clone https://github.com/180D-FW-2020/Team2.git
-
+echo "install IMU packages"
+sudo apt-get install -y git i2c-tools libi2c-dev
+conda install -y -c conda-forge smbus2 
 sudo usermod -a -G spi,gpio pi
+
+echo "Installing python==3.6.1"
+conda install -y python==3.6.1
 
 echo "installing python libraries"
 sudo apt install build-essential python3-dev python3-pip libfreetype6-dev libjpeg-dev libopenjp2-7 libtiff5
@@ -21,9 +21,9 @@ echo "installing setup tools"
 sudo -H pip install --upgrade --ignore-installed pip setuptools
 
 echo "install matrix packages"
-sudo -H pip install --upgrade luma.led_matrix
+pip install luma.led_matrix
 
 echo "install paho for mqtt"
-sudo pip install paho-mqtt
+pip install paho-mqtt
 
 echo "finished rpi installation!"
